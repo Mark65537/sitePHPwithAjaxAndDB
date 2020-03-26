30263e9650d534a77085f1f601b8e2be0f70a763
Mark65537 <55081239+Mark65537@users.noreply.github.com>
2020-03-26 11:25:37 +0300

    Add files via upload

---
+++ 5detail_page.php
@@ -0,0 +1,25 @@
+﻿<!DOCTYPE html>
+<HTML lang="ru">
+ <HEAD>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <TITLE>Главная страница</TITLE>
+  <link rel="stylesheet" href="class1.css"> 
+ <body>
+  
+  <TABLE rules="all" frame="box" width="100%">
+   <TR>
+    <TD> <LI> <A href="index.php">Главная</A> 
+    <TD> <LI> <A href="2news_page.php?page=1">Новости</A>
+	<TD> <LI> <A href="3about_page.php">О компании</A>
+	<TD> <LI> <A href="4kontakt_page.php">Контакты</A>       
+   </TR>
+   </TABLE>
+   
+   <?php
+	if(isset($_GET["new"])){		
+	  $new=$_GET["new"]+1;
+      echo "<h1>Новость {$new}</h1><p>Подробное описание {$new}</p>";
+	}	  
+   ?>
+ </body>
+</HTML>
