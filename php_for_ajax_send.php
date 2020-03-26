819db09470fd668e58201c3988ccd66da27065f3
Mark65537 <55081239+Mark65537@users.noreply.github.com>
2020-03-26 11:25:11 +0300

    Add files via upload

---
+++ php_for_ajax_send.php
@@ -0,0 +1,15 @@
+﻿<?php
+ class mail{
+  var $fio;
+  var $tema;
+  var $text;
+  var $telefon;
+  var $email;
+ }
+  $mymail=new mail;
+  $mymail->$fio=$_GET["fio"];
+  $mymail->$tema=$_GET["tema"];
+  $mymail->$text=$_GET["text"];
+  $mymail->$telefon=$_GET["telefon"];
+  $mymail->$email=$_GET["email"];
+ echo mail($mymail->$email, $mymail->$tema, $mymail->$text.'/n'.$mymail->$fio.' '.$mymail->$telefon);
