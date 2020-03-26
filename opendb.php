dc327375aa9f4092e57dead85fd8a7e10d405f57
Mark65537 <55081239+Mark65537@users.noreply.github.com>
2020-03-26 11:24:56 +0300

    Add files via upload

---
+++ opendb.php
@@ -0,0 +1,12 @@
+﻿<?php
+    $servername = "localhost";
+	$username = "root";
+	$password = "";
+	$database = "news";	
+   $db = mysql_connect ($servername,$username,$password);
+   mysql_select_db ($database,$db);
+   // Ругаемся, если соединение установить не удалось
+   if (!$db) {
+    exit('Не могу соединиться с БД. Код ошибки: ' . mysqli_connect_errno() . ', ошибка: ' . mysqli_connect_error());
+  }
+ ?>
